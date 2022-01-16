db49389382917bc81bfde500fdcdde45fd136798
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2022-01-16 15:21:24 -0300

    feat(user): Users#Update, also its tests, added. (#10)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :set_user, only: :destroy
+      before_action :set_user, only: %i[update destroy]
 
       def create
         @user = User.new(user_params)
@@ -11,6 +11,14 @@ module Api
 
         if @user.save
           render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          render json: @user, status: :ok
         else
           render json: @user.errors, status: :unprocessable_entity
         end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -5,6 +5,7 @@ require 'byebug'
 RSpec.describe 'api/v1/users', type: :request do
   let(:user) { create(:user) }
   let(:valid_attributes) { attributes_for(:user) }
+  let(:invalid_attributes) { valid_attributes.merge(email: '') }
   let(:valid_headers) {}
 
   describe 'POST /create' do
@@ -29,6 +30,47 @@ RSpec.describe 'api/v1/users', type: :request do
 
       it 'renders a JSON response with the new user' do
         valid_post_request
+        expect(response.content_type).to match(a_string_including('application/json'))
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      it 'updates the requested user' do
+        user
+        patch api_v1_user_url(user),
+              params: { name: 'NewUser' }, headers: valid_headers, as: :json
+        user.reload
+      end
+
+      it 'renders a JSON response with the user' do
+        existing_user = user
+        patch api_v1_user_url(existing_user),
+              params: { name: 'NewUser' }, headers: valid_headers, as: :json
+        expect(response.content_type).to match(a_string_including('application/json'))
+      end
+
+      it 'response a :ok http code' do
+        existing_user = user
+        patch api_v1_user_url(existing_user),
+              params: { name: 'NewUser' }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'response an :unprocessable_entity http code' do
+        existing_user = user
+        patch api_v1_user_url(existing_user),
+              params: invalid_attributes, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'renders a JSON response with errors for the user' do
+        existing_user = user
+        patch api_v1_user_url(existing_user),
+              params: invalid_attributes, headers: valid_headers, as: :json
         expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
