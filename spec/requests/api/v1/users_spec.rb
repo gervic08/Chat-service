dc4d9eb0e8aef40f016ea405e6c33f71f9347f58
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2022-01-16 12:48:56 -0300

    feat(user): User Model, Controller#create and its tests, added. (#6)

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,7 +4,35 @@ require 'rails_helper'
 require 'byebug'
 RSpec.describe 'api/v1/users', type: :request do
   let(:user) { create(:user) }
+  let(:valid_attributes) { attributes_for(:user) }
   let(:valid_headers) {}
+
+  describe 'POST /create' do
+    let(:valid_post_request) do
+      post api_v1_users_url,
+           params: valid_attributes,
+           headers: valid_headers,
+           as: :json
+    end
+
+    context 'with valid parameters' do
+      it 'response with :created http code' do
+        valid_post_request
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'creates a new User' do
+        expect do
+          valid_post_request
+        end.to change(User, :count).by(1)
+      end
+
+      it 'renders a JSON response with the new user' do
+        valid_post_request
+        expect(response.content_type).to match(a_string_including('application/json'))
+      end
+    end
+  end
 
   describe 'DELETE /destroy' do
     it 'destroys the requested user' do
